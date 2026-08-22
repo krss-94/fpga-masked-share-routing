89b0cc1f075b9259098ff199638bb4cf2a8cd5b1
Siva Srinivas <113835100+krss-94@users.noreply.github.com>
2026-08-22 12:38:03 +0530

    Delete scripts directory

--- scripts/lut_count_v2.tcl
+++
@@ -1,2 +0,0 @@
-open_checkpoint ./test3_place_route_v2_out/post_route.dcp
-puts "v2 LUT count: [llength [get_cells -hierarchical -filter {REF_NAME =~ LUT*}]]"
--- scripts/lut_inventory_baseline.tcl
+++
@@ -1,8 +0,0 @@
-open_checkpoint ../test3_place_route_out/post_route.dcp
-set fh [open "./lut_inventory_baseline.rpt" w]
-puts $fh "cell,ref_name,site,bel"
-foreach c [get_cells -hierarchical -filter {REF_NAME == LUT1 || REF_NAME == LUT2 || REF_NAME == LUT3 || REF_NAME == LUT4 || REF_NAME == LUT5 || REF_NAME == LUT6}] {
-    puts $fh "${c},[get_property REF_NAME $c],[get_property SITE $c],[get_property BEL $c]"
-}
-close $fh
-puts "Baseline LUT count: [llength [get_cells -hierarchical -filter {REF_NAME =~ LUT*}]]"
